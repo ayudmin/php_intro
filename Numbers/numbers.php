5b1dc0ddf154061b5cb216475e51f57e9c007017
francis556 <vikrammehra244@gmail.com>
2022-01-18 20:07:04 +0200

    numbers

---
+++ Numbers/numbers.php
@@ -0,0 +1,25 @@
+<?php 
+
+print 56;
+print '<br>';
+print 0.0;
+
+
+print '<br>';
+print '<br>';
+
+print 2 + 2;
+print '<br>';
+print '<br>';
+print 17 - 3.5;
+print '<br>';
+print '<br>';
+print 2 * 6;
+print '<br>';
+print '<br>';
+print 10 / 2;
+print '<br>';
+print '<br>';
+print 17 % 3;
+
+?>
