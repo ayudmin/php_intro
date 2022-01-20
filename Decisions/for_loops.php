d9e0db7a8a4ea3ca1f5fa9154ec239b7f1cdb904
francis556 <vikrammehra244@gmail.com>
2022-01-20 21:47:25 +0200

    multiple expression in for loops

--- Decisions/for_loops.php
+++ Decisions/for_loops.php
@@ -7,4 +7,18 @@ for ($i = 1; $i <= 10; $i++){
 
 print '</select>';
 
+// multiple expresion in for loops
+
+print '<br>';
+print '<br>';
+print '<select name="doughnuts">';
+
+for ($min = 1, $max = 10; $min < 50; $min += 10, $max += 10 ){
+    print "<option>$min - $max</option>";
+
+}
+
+print '</select>';
+
+
 ?>
