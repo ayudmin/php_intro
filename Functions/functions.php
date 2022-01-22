3162d3a95570a5a8bb343e81d12e68db826e662c
francis556 <vikrammehra244@gmail.com>
2022-01-22 18:04:56 +0200

    function calling

--- Functions/functions.php
+++ Functions/functions.php
@@ -3,7 +3,20 @@
 // declaring functions
 
 function page_header(){
-    print "Hello world functions";
+    print '<html><head><title>Welcome to my site</title></head>';
+    print '<body bg="#ffffff">';
+}
+
+page_header();
+print 'Welcome, $user';
+page_footer();
+
+
+function page_footer(){
+    print '<br>';
+    print '<br>';
+    print 'No time, make it.';
+    print '</body></html>';
 }
 
 ?>
