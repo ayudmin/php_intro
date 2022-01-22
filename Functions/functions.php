ac52fe31305b38d95426d5eadd1baeabb52744c8
francis556 <vikrammehra244@gmail.com>
2022-01-22 18:36:42 +0200

    function arguments and default values

--- Functions/functions.php
+++ Functions/functions.php
@@ -1,13 +1,15 @@
 <?php 
 
 // declaring functions
+// function arguments
+// default values for arguments
 
-function page_header(){
+function page_header($color = 'red'){
     print '<html><head><title>Welcome to my site</title></head>';
-    print '<body bg="#ffffff">';
+    print '<body bg="#' . $color . '">';
 }
 
-page_header();
+page_header('cc3399');
 print 'Welcome, $user';
 page_footer();
 
@@ -18,5 +20,9 @@ function page_footer(){
     print 'No time, make it.';
     print '</body></html>';
 }
+
+
+
+
 
 ?>
