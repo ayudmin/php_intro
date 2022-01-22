0cb18d75b20088140e8cabced2c824648cf4dca4
francis556 <vikrammehra244@gmail.com>
2022-01-22 18:41:58 +0200

    multiple arguments

--- Functions/functions.php
+++ Functions/functions.php
@@ -3,13 +3,14 @@
 // declaring functions
 // function arguments
 // default values for arguments
+// multiple arguments
 
-function page_header($color = 'red'){
-    print '<html><head><title>Welcome to my site</title></head>';
+function page_header($color = 'red', $title = 'my 1site'){
+    print '<html><head><title>Welcome to ' . $title . '</title></head>';
     print '<body bg="#' . $color . '">';
 }
 
-page_header('cc3399');
+page_header('cc3399',"francis' blog");
 print 'Welcome, $user';
 page_footer();
 
