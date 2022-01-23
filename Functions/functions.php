2d2a1c8b938905e6bc87a17c2b6017beca4c64d8
francis556 <vikrammehra244@gmail.com>
2022-01-23 05:39:08 +0200

    returning values from a function

--- Functions/functions.php
+++ Functions/functions.php
@@ -6,6 +6,9 @@
 // multiple arguments
 // Multiple optional arguments
 // changing argument values
+//returning value from a function
+
+
 
 function page_header($page_id, $color = 'red', $title = 'my 1site'){
     print '<html><head><title>Welcome to ' . $title . '</title></head>';
@@ -36,5 +39,17 @@ function countdown($top){
 $counter = 5;
 countdown($counter);
 
+
+function restaurant_check($meal,$tax,$tip){
+    $tax_amount = $meal * ($tax / 100);
+    $tip_amount = $meal * ($tip / 100);
+
+    $total_amount = $meal + $tax_amount + $tip_amount;
+    return $total_amount;
+}
+
+$price = restaurant_check(500,20,10);
+print '<br>';
+print "$" . "$price";
 
 ?>
