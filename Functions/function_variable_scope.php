9bfdfb9f564bc3e92eb75c650592272d511880c8
francis556 <vikrammehra244@gmail.com>
2022-01-24 14:17:22 +0200

    global keyword

--- Functions/function_variable_scope.php
+++ Functions/function_variable_scope.php
@@ -2,6 +2,8 @@
 
 // variable scope
 // $GLOBALS array
+// modifying a variable with $BLOBALS
+// global keyword
 
 $dinner =   'Curry Cuttlefish';
 
@@ -46,5 +48,37 @@ print '<br>';
 microbiotic_dinner();
 print '<br>';
 print "Regular dinner is $dinner";
+
+
+function hungry_dinner(){
+    $GLOBALS['dinner'] .= ' and Deep-Fried Taro';
+}
+
+print '<br>';
+print '<br>';
+print "$dinner 1" ;
+print '<br>';
+print '<br>';
+hungry_dinner();
+print '<br>';
+print '<br>';
+print "$dinner 2" ;
+
+
+$dinner = 'Chappati';
+
+function korofo_dinner(){
+    global $dinner;
+    print '<br>';
+    print '<br>';
+    print "Dinner was $dinner, but now it is";
+    $dinner = 'Korofo';
+    print '<br>';
+    print '<br>';
+    print $dinner;
+}
+
+
+korofo_dinner()
 
 ?>
