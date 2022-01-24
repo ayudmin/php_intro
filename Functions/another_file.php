2d74cd25e2bce901438bd9051bd9f0e318ef6862
francis556 <vikrammehra244@gmail.com>
2022-01-24 15:36:35 +0200

    Referencing a separate file

--- Functions/another_file.php
+++ Functions/another_file.php
@@ -5,9 +5,15 @@
 require 'restaurants-functions.php';
 
 
+/* $25 check, plus 8.87% tax, plus 20% tip */
 
+$total_bill = restaurant_check(25, 8.87, 20);
 
+/* i have got $30 */
 
+$cash = 30;
+
+print "I need to pay with " . payment_method($cash, $total_bill);
 
 
 ?>
