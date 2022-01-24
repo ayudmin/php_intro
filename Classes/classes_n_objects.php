7823ba07584beae1a40a7aba7d0fe98f2b850d40
francis556 <vikrammehra244@gmail.com>
2022-01-24 21:14:37 +0200

    Creating and using objects

--- Classes/classes_n_objects.php
+++ Classes/classes_n_objects.php
@@ -2,17 +2,36 @@
 
 
 // defining classes
+// creating objects from classes
 
 class Entree{
     public $name;
     public $ingredients = array();
 
-    public function hasIngredients($ingredients){
-        return in_array($ingredients, $this->ingredients);
+    public function hasIngredient($ingredient){
+        return in_array($ingredient, $this->ingredients);
     }
 }
 
 
+$soup = new Entree;
+$soup->name = 'Chicken Soup';
+$soup->ingredients = array('Chicken','water');
 
+$sandwitch = new Entree;
+$sandwitch->name = 'Chicken Sandwich';
+$sandwitch->ingredients = array('Chicken', 'bread');
+
+
+foreach (['Chicken','water','bread','lemon'] as $ing ){
+    if ($soup->hasIngredient($ing)){
+        print "Soup contains $ing.";
+        print '<br>';
+    }
+    if ($sandwitch->hasIngredient($ing)){
+        print "Sandwitch contains $ing.";
+        print '<br>';
+    }
+}
 
 ?>
