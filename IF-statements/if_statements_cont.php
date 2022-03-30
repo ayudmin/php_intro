f1d90c1ac7b85e4fb97ec26feee84529690de2d7
francis556 <vikrammehra244@gmail.com>
2022-03-30 15:52:52 +0200

    3 comparison

--- IF-statements/if_statements_cont.php
+++ IF-statements/if_statements_cont.php
@@ -9,18 +9,23 @@
 <body>
     <?php 
     
-        function getmax($num1, $num2){
+        function getmax($num1, $num2, $num3){
 
-            if ($num1 > $num2){
+            if ($num1 >= $num2 && $num1 >= $num3){
                 
                 return $num1;
-            } else {
+
+            }elseif($num2 >= $num1 && $num2 >= $num3){
+
                 return $num2;
+
+            } else {
+                return $num3;
             }
         }
         
 
-        echo getmax(300 , 100);
+        echo getmax(300 , 1010000, 10000);
     ?>
 </body>
 </html>
