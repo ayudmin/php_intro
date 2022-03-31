1f7705c4df931579db83a3d481027b8b1d742f38
francis556 <vikrammehra244@gmail.com>
2022-03-31 22:53:00 +0200

    switch.php

---
+++ FLOW_CONTROL/IF-statements/if_statements.php
@@ -0,0 +1,20 @@
+<?php 
+
+$is_male = FALSE;
+$is_tall = FALSE;
+
+if ($is_male && $is_tall){
+    echo "You are a tall male";
+} elseif ($is_male && !$is_tall){
+
+    echo "You are a short male";
+}
+ elseif (!$is_male && $is_tall){
+
+    echo "You are a tall female";
+
+ } else {
+    echo "You are not male nor tall";
+}
+
+?>  
