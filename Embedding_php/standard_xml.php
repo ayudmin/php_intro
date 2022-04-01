1d6e2e1697df132e253f46a665e5fca7239fb600
francis556 <vikrammehra244@gmail.com>
2022-04-01 05:11:33 +0200

    embedding php using standard xml method

---
+++ Embedding_php/standard_xml.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>This is my first PHP program</title>
+</head>
+<body>
+    
+    <p>Look, ma! It's my first PHP program:<br/>
+    <?php echo "Hello, world!";?><br/>
+    How cool is that!
+    </p>
+
+</body>
+</html>
