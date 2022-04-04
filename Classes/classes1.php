7460a09d032994a48577faf9774394385d345ff2
francis556 <vikrammehra244@gmail.com>
2022-04-04 05:37:55 +0200

    constructor

--- Classes/classes1.php
+++ Classes/classes1.php
@@ -6,12 +6,15 @@ class Book
     var $title;
 
     public $author;
+
+    function __construct($title, $author){
+        $this->title = $title;
+        $this->author = $author;
+    }
 }
 
     
-$book1 = new Book;
-$book1->title = "Hary Potter";
-$book1->author = "Ayume Francis";
+$book1 = new Book("Hary Potter", "Ayume Francis" );
 
 echo $book1->author;
 
